349bf6b8635b3886a8f6c71071edafe8476446d8
Aanval2011 <126170908+Aanval2011@users.noreply.github.com>
2023-02-23 14:27:40 +0100

    hij is sneller

--- main.ts
+++ main.ts
@@ -4,13 +4,13 @@ let R = 0
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 5)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
     } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 5)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 30)
     }
 })
 basic.forever(function () {
